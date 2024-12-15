d50c0135b7001bc75d75d7c4c2fe49f2cc40ac08
isur <isuru.jayarathne@gmail.com>
2024-12-15 11:40:36 +0900

    adding files

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+st.set_page_config(page_icon="🎓", page_title="Leaner App")
+
+
+def get_upload(uploaded_file):    
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+def main():    
+    st.header("Machine Learning UI")
+    with st.container(border=True):
+        st.markdown("### Start by uploading your dataset.")
+        with st.expander("What should be the structure?"):
+            st.write('''
+                The chart above shows some numbers I picked for you.
+                I rolled actual dice for these, so they're *guaranteed* to
+                be random.
+            ''')
+        uploaded_file = st.file_uploader(label="Upload a CSV file", key="dataset_uploder", type=["csv"])
+        if uploaded_file is not None:
+            get_upload(uploaded_file)
+
+    col1, col2 = st.columns(2)
+
+
+main()

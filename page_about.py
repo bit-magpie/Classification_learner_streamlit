257db8dacd8a5fe52225c34af0fe6c32d1992e4c
Isuru Jayarathne <isuru.jayarathne@gmail.com>
2024-12-27 21:56:36 +0900

    Update page_about.py

--- page_about.py
+++ page_about.py
@@ -8,5 +8,6 @@ def main():
         with st.container():
             st.write("#### Classic Learner V1.0.0")
             st.write("**Author:** Isuru Jayarathne")
+            st.write("**Git repo:** [https://github.com/bit-magpie/Classification_learner_streamlit](https://github.com/bit-magpie/Classification_learner_streamlit)")
 
 main()
